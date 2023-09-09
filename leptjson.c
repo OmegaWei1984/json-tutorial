d851dd9a4e218a508455590489c1d4c82664fe67
OmegaWei1984 <omegawei1984@gmail.com>
2023-09-09 22:03:31 +0800

    feat: 增加 lept_parse_number

--- leptjson.c
+++ leptjson.c
@@ -46,6 +46,16 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
 	return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context* c, lept_value* v) {
+	char* end;
+	v->n = strtod(c->json, &end);
+	if (c->json == end)
+		return LEPT_PARSE_INVALID_VALUE;
+	c->json = end;
+	v->type = LEPT_NUMBER;
+	return LEPT_PARSE_OK;
+}
+
 /* value = null / false / true */
 static int lept_parse_value(lept_context* c, lept_value* v) {
 	switch (*c->json) {
@@ -53,7 +63,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 		case 't':  return lept_parse_true(c, v);
 		case 'f':  return lept_parse_false(c, v);
 		case '\0': return LEPT_PARSE_EXPECT_VALUE;
-		default:   return LEPT_PARSE_INVALID_VALUE;
+		default:   return lept_parse_number(c, v);
 	}
 }
 
